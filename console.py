3c23696cf47e7d73a5ef52a157231462fc54221c
div-styl <gouhmada@gmail.com>
2023-08-07 17:31:20 +0100

    lots of issues

--- console.py
+++ console.py
@@ -3,6 +3,11 @@
 import cmd
 from models.base_model import BaseModel
 from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
 
 
 def prase_arg(self, arg):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,3 +8,38 @@ from models.city import City
 from models.review import Review
 from models.amenity import Amenity
 from models.place import Place
+
+
+class FileStorage:
+    """define the FileStorage class"""
+    __file_path = "file.json"
+    __objects = {}
+    
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        new_dict = {}
+        for key, value in FileStorage.__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as f:
+            json.dump(new_dict, f)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        try:
+            with open(FileStorage.__file_path, mode="r", encoding="utf-8") as f:
+                new_dict = json.load(f)
+            for key, value in new_dict.items():
+                class_name = value["__class__"]
+                del value["__class__"]
+                FileStorage.__objects[key] = eval(class_name)(**value)
+        except FileNotFoundError:
+            pass
