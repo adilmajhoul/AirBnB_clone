77fd06a59c5f5d97589b6da3bc56dc11d4d7a25e
div-styl <gouhmada@gmail.com>
2023-08-07 17:06:50 +0100

    do_create cmd added

--- console.py
+++ console.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """defines the HBNBCommand class"""
 import cmd
-
+from models.base_model import BaseModel
 class HBNBCommand(cmd.Cmd):
     """represents the entry point of the command interpreter"""
     prompt = '(hbnb) '
@@ -25,6 +25,28 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """empty line + ENTER shouldn’t execute anything"""
         pass
+
+    def prase_arg(self, arg):
+        """parse the arg"""
+        args = arg.split()
+        if len(args) == 0:
+            return None
+        if args[0] not in self.__models_classes:
+            return None
+        return args[0]
+    
+    def do_create(self, arg):
+        """create a new instance of BaseModel and prints the id"""
+        model = self.prase_arg(arg)
+        if model is None:
+            print("** class name missing **")
+            return
+        obj = eval(model)()
+        obj.save()
+        print(obj.id)
+
+    
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
