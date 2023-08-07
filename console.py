cf26c63d6ce89a36f3796383087f0848e09f9df2
div-styl <gouhmada@gmail.com>
2023-08-07 16:42:37 +0100

    the console has set but it doesn't fully work

--- console.py
+++ console.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""defines the HBNBCommand class"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """represents the entry point of the command interpreter"""
+    prompt = '(hbnb) '
+    __models_classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """empty line + ENTER shouldn’t execute anything"""
+        pass
+
+
+
+if __name__ == "__main__":
+    HBNBCommand.cmdloop()
