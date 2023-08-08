dfa990adb2f5bee2eadda8ae8fda443d09675147
div-styl <gouhmada@gmail.com>
2023-08-08 19:01:49 +0100

    destroy works perfectly

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,12 @@
 
 import json
 from pathlib import Path
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models.base_model import BaseModel
 
 
