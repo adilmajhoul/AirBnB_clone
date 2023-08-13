fdfd07d68c63b07430e88d9c4d6b414ca83de452
div-styl <gouhmada@gmail.com>
2023-08-13 19:23:58 +0100

    lemme see the city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,44 +1,13 @@
 #!/usr/bin/python3
+import json
 import unittest
-from models.engine.file_storage import FileStorage
-from models.city import City
-from models import storage
+from models.base_model import BaseModel
+from datetime import datetime
 import pycodestyle
-""" city class unit test """
-
-
-class TestCity(unittest.TestCase):
-    """ city class unit test """
-
-    def setUp(self):
-        self.city = City()
-        self.city.name = "San Francisco"
-        self.city.state_id = "CA"
-
-    def tearDown(self):
-        del self.city
-
-    def test_save_reload_city(self):
-        """ test that a City object can be saved and reloaded correctly """
-        storage.new(self.city)
-        storage.save()
-
-        new_storage = FileStorage()
-        new_storage.reload()
-
-        new_objects = new_storage.all()
-        self.assertIn(f"City.{self.city.id}", new_objects)
-        new_city = new_objects[f"City.{self.city.id}"]
-        self.assertEqual(new_city.name, self.city.name)
-        self.assertEqual(new_city.state_id, self.city.state_id)
-
-    def test_reload(self):
-        """ ensure that reload works """
-        len_before = len(storage.all())
-        storage.new(City())
-        storage.reload()
-        len_after = len(storage.all())
-        self.assertEqual(len_after, len_before + 1)
+import models
+from io import StringIO
+import sys
+from unittest.mock import patch
 
 
 class Testcodestyle(unittest.TestCase):
